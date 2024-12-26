f52ec08f7b02a3f6faffcc69236082f840b753c5
Salomo309 <salomo.manalu309@gmail.com>
2024-12-26 14:28:43 +0700

    feat: add oracle routes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const googleDriveRouter = require("./api/googleDriveRoutes");
 const githubRouter = require("./api/githubRoutes");
+const oracleRouter = require("./api/oracleRoutes");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,9 +15,9 @@ app.use(fileUpload({
   tempFileDir: "/tmp/"
 }));
 
-// Route untuk upload file
 app.use("/api", googleDriveRouter);
 app.use("/api", githubRouter);
+app.use("/api", oracleRouter);
 
 // Route default
 app.get("/", (req, res) => {
